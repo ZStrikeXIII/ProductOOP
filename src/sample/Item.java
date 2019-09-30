1ab0d7664ecf9face05347f26a0db3db6bb60699
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-09-30 19:12:01 -0400

    2.0: Added code in Item

--- src/sample/Item.java
+++ src/sample/Item.java
@@ -2,4 +2,13 @@ package sample;
 
 public interface Item {
 
+  public int getInt();
+
+  public void setName(String n);
+
+  public String getName();
+
+  public void setManufacturer(String mfact);
+
+  public String getManufacturer();
 }

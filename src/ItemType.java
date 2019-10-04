c86a042803e39b0b249b1de6f98f72e9f6fd13a1
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-10-04 09:40:23 -0400

    3.0: Added more code to ItemType. Added abstract Product class.

--- src/ItemType.java
+++ src/ItemType.java
@@ -1,2 +1,14 @@
 public enum ItemType {
+  AU("Audio", "AU"),
+  VI("Visual","VI"),
+  AM("AudioMobile", "AM"),
+  VM("VisualMobile", "VM");
+
+  private final String type;
+  private final String code;
+
+  ItemType(String type, String code){
+    this.type = type;
+    this.code = code;
+  }
 }
---
+++ src/sample/Product.java
@@ -0,0 +1,10 @@
+package sample;
+
+public abstract class Product {
+  int id;
+  String type;
+  String manufacturer;
+  String name;
+
+
+}
